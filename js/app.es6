05946e4c1ce24f781822083854b6e3fa877bc96c
Matthew Morgan <matt@dulcetsoftware.com>
2016-11-24 09:03:58 -0500

    Separate CSS/SCSS

--- js/app.es6
+++ js/app.es6
@@ -1,5 +1,8 @@
 import { login } from './login';
 
+require('../css/bootstrap.css');
+require('../css/app.scss');
+
 login('admin', 'radical');
 
 document.write("Welcome to Big Hair Concerts Baby!!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 var path = require('path');
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   context:   path.resolve('js'),
-  entry:     ["./utils", './app'],
+  entry:     ['./app'],
   output:    {
-    path:       path.resolve('build/js/'),
-    publicPath: '/public/assets/js/',
-    filename:   './bundle.js'
+    path:       path.resolve('build/'),
+    publicPath: 'public/assets/',
+    filename:   'mainBundle.js'
   },
+  plugins:    [
+    new ExtractTextPlugin("styles.css")
+  ],
   devServer: {
     contentBase: 'public'
   },
@@ -20,6 +25,18 @@ module.exports = {
       }
     ],
     loaders:    [
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
+      {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
+      {
+        test:    /\.css$/,
+        exclude: /node_modules/,
+        loader:  ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test:    /\.scss$/,
+        exclude: /node_modules/,
+        loader:  ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+      },
       {
         test:    /\.es6$/,
         exclude: /node_modules/,
